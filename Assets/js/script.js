0d8092116299190783a7a3341cadf46cee558048
Carrie Young <carrieky04@gmail.com>
2022-11-04 01:26:49 -0700

    adds working api requests

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,39 @@
 var searchBtn = document.querySelector('#search-btn');
-console.log('test1')
-function getApi(event) {
+var currentWeather = document.querySelector('.container-fluid');
+
+// console.log('test1')
+function getWeather(event) {
     event.preventDefault();
     var inputEl = document.querySelector('#exampleFormControlInput1');
     var cityName = inputEl.value;
 
-    console.log("in getAPI()");
-    // var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7dd3c97591974f9fd579b18818499b7c";
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=3&appid=7dd3c97591974f9fd579b18818499b7c"
-    fetch(requestUrl)
+    // console.log("in getAPI()");
+    
+    var requestCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=3&appid=7dd3c97591974f9fd579b18818499b7c"
+    fetch(requestCity)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-        }
-    )
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            console.log(lat)
+            console.log(lon);
+            var requestLatLon = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=7dd3c97591974f9fd579b18818499b7c";
+                fetch(requestLatLon)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then (function (data) {
+                        console.log(data);
+                        var { temp } = list.main;
+                        console.log(temp);
+                    })                   
+        })  
 }
 
-searchBtn.addEventListener("click", getApi);
+searchBtn.addEventListener("click", getWeather);
+
+
+
